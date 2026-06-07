f53f0dad0f2dd10c3b645c84b0922605e29d13ba
AlperenGurdal <alpeni4848@gmail.com>
2026-06-07 07:27:14 -0400

    added editing expenses

--- main.py
+++ main.py
@@ -97,8 +97,44 @@ def view_by_category(expenses):
     print("\n----------------------")
     print(f"TOTAL: ${total_spent:.2f}")
 
+def edit_expenses(expenses):
+    if not expenses:
+        print("no expenses to edit")
+        return
     
+    view_expenses(expenses)
+    
+    try:
+        index = int(input("Enter Expense number to edit: "))-1
+    except ValueError:
+        print("invalid input")
+        return
+    
+    expense = expenses[index]
+    print("\nLeave blank to keep current value.")
 
+    description = input(f"Description [{expense['description']}]: ")
+    amount_input = input(f"Amount [{expense['amount']}]: ")
+    category = input(f"description[{expense['category']}]: ")
+
+    if description:
+        expense["description"] = description
+
+    if amount_input:
+        try:
+            expense["amount"] = float(amount_input)
+        except ValueError:
+            print("Invalid amount.")
+            return
+
+    if category:
+        expense["category"] = category
+    
+    save_expenses(expenses)
+    print("Expense updated!")
+
+
+    
 def main():
     expenses = load_expenses()
     while True:
@@ -107,8 +143,9 @@ def main():
         print("2. View Expense")
         print("3. View Total")
         print("4. View Expenses By Category")
-        print("5. Delete Expense")
-        print("6. Exit")
+        print("5. Edit Expense")
+        print("6. Delete Expense")
+        print("7. Exit")
 
         choice = input("choose an option: ")
 
@@ -121,8 +158,10 @@ def main():
         elif choice == "4":
             view_by_category(expenses)
         elif choice == "5":
-            delete_expenses(expenses)
+            edit_expenses(expenses)
         elif choice == "6":
+            delete_expenses(expenses)
+        elif choice == "7":
             print("Goodbye")
             break
         else:
