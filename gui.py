a93755f4f3daedb28271793ad4b244128915500b
AlperenGurdal <alpeni4848@gmail.com>
2026-06-11 20:36:06 -0400

    added GUI

---
+++ gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Finance Tracker")
+window.geometry("600x400")
+
+title = tk.Label(window, text="Finance Tracker")
+title.pack()
+
+window.mainloop()
